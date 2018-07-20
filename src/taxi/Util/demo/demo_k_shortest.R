257d3f4b23bf6511c9b2ba24648777c2774508a4
= <=>
2018-07-20 11:27:15 -0400

    explict error message when trying to find paths from x to x

---
+++ src/taxi/Util/demo/demo_k_shortest.R
@@ -0,0 +1,19 @@
+source('../../Util/k_shortest.R')
+#===================Test==========================#
+edgeList <- tibble(from=character(), to=character(), weight = numeric())
+edgeList[nrow(edgeList)+1,] <-list('c','d',3)
+edgeList[nrow(edgeList)+1,] <-list('d','f',4)
+edgeList[nrow(edgeList)+1,] <-list('f','h',1)
+edgeList[nrow(edgeList)+1,] <-list('c','e',2)
+edgeList[nrow(edgeList)+1,] <-list('e','d',1)
+edgeList[nrow(edgeList)+1,] <-list('e','f',2)
+edgeList[nrow(edgeList)+1,] <-list('e','g',3)
+edgeList[nrow(edgeList)+1,] <-list('g','h',2)
+edgeList[nrow(edgeList)+1,] <-list('f','g',2)
+edgeList[nrow(edgeList)+1,] <-list('a','b',2)
+edgeList[nrow(edgeList)+1,] <-list('c','c',4)
+
+graph <- graph.data.frame(edgeList)
+
+k_shortest.yen(graph, 'c','c',7) #expect error
+k_shortest.yen(graph,'c','h',7)
--- src/taxi/Util/k_shortest.R
+++ src/taxi/Util/k_shortest.R
@@ -74,21 +74,4 @@ paths_to_tibble <- function(graph, paths) {
 k_shortest_path <- function(graph, from, to, k) k_shortest.yen(graph, from, to, k) %>% paths_to_tibble(graph=graph)
   
 
-#===================Test==========================#
-edgeList <- tibble(from=character(), to=character(), weight = numeric())
-edgeList[nrow(edgeList)+1,] <-list('c','d',3)
-edgeList[nrow(edgeList)+1,] <-list('d','f',4)
-edgeList[nrow(edgeList)+1,] <-list('f','h',1)
-edgeList[nrow(edgeList)+1,] <-list('c','e',2)
-edgeList[nrow(edgeList)+1,] <-list('e','d',1)
-edgeList[nrow(edgeList)+1,] <-list('e','f',2)
-edgeList[nrow(edgeList)+1,] <-list('e','g',3)
-edgeList[nrow(edgeList)+1,] <-list('g','h',2)
-edgeList[nrow(edgeList)+1,] <-list('f','g',2)
-edgeList[nrow(edgeList)+1,] <-list('a','b',2)
-edgeList[nrow(edgeList)+1,] <-list('c','c',4)
-
-graph <- graph.data.frame(edgeList)
-
-k_shortest.yen(graph, 'c','c',7) #expect error
-k_shortest.yen(graph,'c','h',7)
+#for demo go to the demo folder 
