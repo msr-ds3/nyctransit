e0f9a2dc7b5cc6225c7085bda6877877e0f5a2cd
Akbar Mirza <akbarbmirza@users.noreply.github.com>
2018-07-26 14:26:10 -0400

    Add Demo For Using Itinerary Eval Functions

---
+++ src/demo/demo_itinerary_eval.R
@@ -0,0 +1,9 @@
+source('../path_finding.R')
+load(file = "../../data/todd_subway_realtime.RData")
+
+example_data <- read_csv('../../data/itineraries-1-train.csv') %>% 
+  filter(event != "travel", itinerary_id < 4) %>%
+  mutate(stop_station = paste(station, direction, sep = ''))
+
+all_itin_df <- compute_all_itins(example_data, subway_data)
+plot_densities(all_itin_df)
