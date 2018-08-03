ddc8c7679f4ff8237fe9c2c574b26388c1e52b70
Phoebe <minhphuong227@gmail.com>
2018-08-03 17:06:19 -0400

    trip generation

--- src/itinerary_evaluation.R
+++ src/itinerary_evaluation.R
@@ -313,9 +313,9 @@ compute_all_itins <- function(cleaned_data, subway_data, start_hour = 7, end_hou
 
 # given a dataframe with multiple itins
 # returns density plot / histogram of time_diffs
-plot_densities <- function(all_itin_df, start_hour = 7 , end_hour = 10, bins = 60, route_color_dict) { 
+plot_densities <- function(all_itin_df, start_hour = 7 , end_hour = 10, bins = 60, route_color_df) { 
   
-  plot_data <- all_itin_df # %>%
+  plot_data <- all_itin_df #%>%
     # mutate(day_of_week = weekdays(leg1_start_time),
     #        hour = hour(leg1_start_time)) %>%
     # filter(isWeekend(day_of_week) == F,
@@ -323,8 +323,37 @@ plot_densities <- function(all_itin_df, start_hour = 7 , end_hour = 10, bins = 6
   
   plot_data$itin_id <- as.factor(plot_data$itin_id)
   
-  colors <- route_color_dict$route_color
-  names(colors) <- route_color_dict$route_short_name
+  colors <- route_color_df$route_color
+  names(colors) <- route_color_df$route_short_name
+  
+   
+  plot_lines <- substr(unique(plot_data$label), 1, 1)
+  plot_colors <- as.vector(colors[plot_lines])
+  
+  plot <- plot_data %>%
+    ggplot(aes(x = as.numeric(time_diff), group=itin_label, fill=itin_label)) +
+    geom_histogram(position = "identity", alpha = 0.5, bins = bins) +
+    labs(title = "Trip Time Evaluation", x = "Trip Time (in minutes)", y = "Number of Trips", fill = "Itinerary\n") +
+    #xlab('Trip Time (in minutes)') +
+    #ylab('Number of Trips') +
+    theme(legend.position = "bottom", legend.direction = "vertical", legend.key.size = unit(1.5, 'lines')) +
+    scale_fill_manual(guide = guide_legend(), values = plot_colors) + 
+    scale_color_manual(values = plot_colors) +
+    xlim(as.numeric(quantile(plot_data$time_diff, c(0.1, 0.9))))
+  # geom_histogram(), position = "identity", alpha = 0.5)
+  
+  return(plot)
+}
+
+plot_facet_hist <- function(all_itin_df, start_hour = 7 , end_hour = 10, bins = 60, route_color_df) { 
+  
+  plot_data <- all_itin_df
+  
+  plot_data$itin_id <- as.factor(plot_data$itin_id)
+  
+  colors <- route_color_df$route_color
+  names(colors) <- route_color_df$route_short_name
+  
   
   plot_lines <- substr(unique(plot_data$label), 1, 1)
   plot_colors <- as.vector(colors[plot_lines])
@@ -332,10 +361,10 @@ plot_densities <- function(all_itin_df, start_hour = 7 , end_hour = 10, bins = 6
   plot <- plot_data %>%
     ggplot(aes(x = as.numeric(time_diff), group=itin_label, fill=itin_label)) +
     geom_histogram(position = "identity", alpha = 0.5, bins = bins) +
-    xlab('Trip Time (in minutes)') +
-    ylab('Number of Trips') +
+    labs(title = "Trip time from Atlantic Ave. station to Fulton Station ", 
+         x = "Trip Time (in minutes)", 
+          y = "Number of Trips", fill = "Itinerary\n") +
     theme(legend.position = "bottom", legend.direction = "vertical", legend.key.size = unit(1.5, 'lines')) +
-    #xlim(as.numeric(quantile(mj_df$time_diff, c(0.1, 0.9)))) +
     scale_fill_manual(guide = guide_legend(), values = plot_colors) + 
     scale_color_manual(values = plot_colors) +
     xlim(as.numeric(quantile(plot_data$time_diff, c(0.1, 0.9))))
