404b006df0fa966f26978108ad28c83bd6456249
Jake Hofman <jhofman@gmail.com>
2018-08-03 11:05:32 -0400

    find figs script

--- figures/find_figs.sh
+++ figures/find_figs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# look at every file in the directory
 for f in *
 do
     echo "looking for figure: $f"
+
+    # find all Rmds in the repo
+    # grep for the filename
     file=`find .. -name '*.Rmd' -exec grep $f {} \;`
+
+    # print based on whether we found the file or not
     if [[ -z $file ]]
     then
 	echo "COULDN'T FIND ANYTHING"
@@ -13,3 +19,8 @@ do
 
     echo
 done
+
+# look at every figure generated from the code
+echo
+echo "figures saved in the code---make sure these are committed and pushed in"
+find .. -name '*.Rmd' -exec grep png {} \;
